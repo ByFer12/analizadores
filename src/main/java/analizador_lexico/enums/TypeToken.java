2422facc5aaaa4f179e7a62b359988906ccea634
Byron Torres <ajxupbyron@gmail.com>
2023-08-10 17:42:45 -0600

    Lectura y guardado de archivo, frontend avanzado, mostrar filas y columnas en el editor

---
+++ src/main/java/analizador_lexico/enums/TypeToken.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package analizador_lexico.enums;
+
+public enum TypeToken {
+    ID,
+    OP_ARITMETIC,
+    OP_COMPARACION,
+    OP_LOGICO,
+    OP_ASIGN,
+    KEY_WORD,
+    CONST,
+    COMENTARIO,
+    OTROS
+    
+}
---
+++ src/main/java/analizador_lexico/utils/IArchive.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package analizador_lexico.utils;
+
+import javax.swing.JFileChooser;
+
+
+public interface IArchive {
+    
+    
+    void chooseFile(JFileChooser file);
+    
+    void saveFile(JFileChooser file);
+    
+    
+}
